9e26bae510b5f9ebf45e5259182cd48f700f0509
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-10-06 16:22:56 +0300

    452342

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,20 @@
 import { Component } from "react";
+import PokemonForm from "./PokemonForm";
 
 export class App extends Component {
-  state = {};
+  state = { pokemonName: "" };
+
+  handleFormSubmit = (pokemonName) => {
+    this.setState(pokemonName);
+  };
 
   render() {
-    return <div></div>;
+    return (
+      <div style={{ maxWidth: 1170, margin: "0 auto", padding: 20 }}>
+        {/* {this.state.loading && <h1>Загружаем.....</h1>}
+        {this.state.pokemon && <div>{this.state.pokemon.name}</div>} */}
+        <PokemonForm onSubmit={this.handleFormSubmit} />
+      </div>
+    );
   }
 }
